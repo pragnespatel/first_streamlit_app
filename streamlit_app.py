e4bcbb2712e639bc21b3eb197f86286608495ade
pragnespatel <151804825+pragnespatel@users.noreply.github.com>
2023-11-24 15:16:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,10 @@ try:
 except URLError as e:
   streamlit.error()
 
-
+def insert_row_snowflake(new_fruit):
+      with my_cnx.cursor() as my_cur:
+            my_cur.execute("insert into fruit_load_list values ('from streamlist')")
+            return "Thanks for adding " + new_fruit
 
 
 
@@ -68,10 +71,23 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
+
+# my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_row)
+# Snowflake-related functions
+def get_fruit_load_list():
+       with my_cnx.cursor() as my_cur:
+             my_cur.execute("SELECT * from fruit_load_list")
+             return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows=get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+             
+             
+# streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?','Kiwi')
 streamlit.write('Thanks for adding', add_my_fruit)
